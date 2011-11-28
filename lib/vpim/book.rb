a26584128f95fcbe8e8f438e31d74b675e094a1f
Josh Starcher <josh.starcher@gmail.com>
2011-11-28 16:09:17 -0500

    remove commented lines

--- lib/vpim/book.rb
+++ lib/vpim/book.rb
@@ -10,14 +10,11 @@ module Vpim
   
     def to_s
   	  @string = ''
-      # @string += "BEGIN:VCARD\n"
       if @vcards.length > 0
-        # @string += @vcards.first.fields[1].encode
         @vcards.each do |vcard|
           @string += vcard.fields.collect { |f| f.encode }.join
         end
       end
-      # @string += "END:VCARD\n"
       @string
     end
   end
